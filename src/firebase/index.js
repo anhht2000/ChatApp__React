529529a66ce18b89888375fe6095bb3b5072782b
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-07-26 17:39:53 +0700

    fix firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -20,9 +20,9 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 
 //emulator la tao cac account gia
-auth.useEmulator("http://localhost:9099");
-if (window.location.hostname === "localhost") {
-  db.useEmulator("localhost", "8080");
-}
+// auth.useEmulator("http://localhost:9099");
+// if (window.location.hostname === "localhost") {
+//   db.useEmulator("localhost", "8080");
+// }
 
 export { auth, db };
